1123c738e4a4985e3294bb9728f068424c294acc
Tanish Sharma <tanish989130@gmail.com>
2026-05-05 17:19:51 +0530

    update api url for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ 
+  baseURL: 'https://taskflow-production-fc88.up.railway.app/' 
+})
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
