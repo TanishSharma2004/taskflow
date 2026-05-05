6ad141eaa13efa14672208e90d80eb258ea472cf
Tanish Sharma <tanish989130@gmail.com>
2026-05-05 18:04:39 +0530

    fix api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({ 
-  baseURL: 'https://taskflow-production-fc88.up.railway.app/' 
+  baseURL: 'https://taskflow-production-fc88.up.railway.app/api'
 })
 
 api.interceptors.request.use(config => {
